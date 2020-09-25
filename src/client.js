a258ac4a39365b144c67cf61cfefff51c731f0a6
rchrdnsh <rchrdnsh@gmail.com>
2020-09-25 13:41:01 -0700

    trying instant routes

--- src/client.js
+++ src/client.js
@@ -1,9 +1,3 @@
-import * as sapper from '@sapper/app';
-
-sapper.start({
-	target: document.querySelector('#sapper')
-});
-
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
@@ -14,3 +8,11 @@ window.addEventListener('resize', () => {
 	let vh = window.innerHeight * 0.01;
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
+
+import * as sapper from '@sapper/app';
+
+sapper.start({
+	target: document.querySelector('#sapper')
+});
+
+sapper.prefetchRoutes();
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -9,10 +9,39 @@
 		padding: 0;
 		width: 100%;
 		height: 100%;
+		/* display: grid; */
+		/* grid-auto-flow: row; */
+		/* place-items: center; */
+		/* align-items: center; */
+		/* justify-content: center; */
+		/* row-gap: 1rem; */
 		display: flex;
 		flex-direction: column;
+		/* place-items: center; */
 		align-items: center;
+		/* justify-items: center; */
 		justify-content: center;
+		gap: 2rem;
+	}
+
+	.demo {
+		background: #fff;
+		height: 300px;
+		width: 400px;
+		margin: 0 auto 50px;
+		padding: 0;
+		display: flex;
+	}
+	two-up {
+		flex: 1;
+	}
+	.light, .dark {
+		text-align: center;
+		padding: 10px 0 0;
+	}
+	.dark {
+		background: #555;
+		color: #ccc;
 	}
 </style>
 
@@ -21,8 +50,23 @@
 </svelte:head>
 
 <div class='container'>
+
 	<h1>About this site</h1>
+
 	<p>This is the 'about' page. There's not much here.</p>
+
 	<Tabs items={[ `Ya`, `Ba`, `Che`, `Go` ]}/>
+
+	<div class="demo">
+		<two-up>
+			<div class="light">
+				<h1>The Light Side</h1>
+			</div>
+			<div class="dark">
+				<h1>The Dark Side</h1>
+			</div>
+		</two-up>
+	</div>
+
 </div>
 
